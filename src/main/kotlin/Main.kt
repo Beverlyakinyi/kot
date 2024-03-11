12df72e2c32506348691e29374104985e96ad6f0
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-03-11 14:50:59 +0300

    added file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main(){
+println(calculateAverage(68,22))
+
+}
+//Write a Kotlin function named calculateAverage that takes a variable number of integers as parameters and returns their average. Print the result.
+fun calculateAverage(num1:Int,num2:Int) : Int{
+    var a=num1 + num2
+    var b= a/2
+    return b
+
+}
+// Create a Kotlin function named isPrime that takes an integer as a parameter and returns true if it's a prime number and false otherwise. Print the result.
+//
+//
+fun isPrime(num1:Int): Int{
+    var check=num1
+    if (check==num1){
+        println("true")}
+    else{
+        println("false")
+    }
+
+    }
+
+
+
